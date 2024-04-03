a736f8ed0c6231bb6b14b98a04bb248db0aef90f
Yin <doyin@deloitte.com.cn>
2024-04-03 14:23:52 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,8 @@
+const reverseString = require('./reverseString')
+const formatDate = require('./formatDate')
+const generateUUID = require('./generateUUID')
+const numberArrayToString = require('./numberArrayToString')
+
+module.exports = (arr) => {
+    return Math.max(...arr);
+  }
